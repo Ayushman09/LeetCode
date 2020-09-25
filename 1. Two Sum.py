2cb7b6b1bdbc734ab62ee86bdd2d170368b582c2
Ayushman Chakravarty <ayushmanchak@gmail.com>
2020-09-25 16:11:16 +0530

    Update and rename 1. Two Sum to 1. Two Sum.py

--- 1. Two Sum.py
+++ 1. Two Sum.py
@@ -5,10 +5,7 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        """for x in range(0,len(nums)):
-            for y in range(x+1,len(nums)):
-                if (nums[x]+nums[y]==target):
-                    return x,y"""
+        
         x = {}
         for i,num in enumerate(nums):
             z = target - num
