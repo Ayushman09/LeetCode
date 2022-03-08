bcac282f50f8f846bcef819515a631b68711d85b
Ayushman09 <ayushmanchak@gmail.com>
2022-03-08 12:12:12 +0530

    March 1-3

---
+++ 413.Arithmetic Slices.py
@@ -0,0 +1,20 @@
+from functools import cache
+
+def numberOfArithmeticSlices(self, nums: List[int]) -> int:
+        @cache
+        def count(n):
+            if cnt < 3: return 0
+            return n - 2 + count(n - 1)
+        
+        prev, cnt, ans = None, 2, 0
+        for i in range(1, len(nums)):
+            diff = nums[i-1] - nums[i]
+            if diff == prev:
+                cnt += 1
+            else:
+                ans += count(cnt)
+                cnt = 2
+                
+            prev = diff
+            
+        return ans + count(cnt)
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # LeetCode
 Solving leetcode problems. All using Java and sometimes I will add Python alternative for some of them.
-
+Basically I will solve the Daily leetcoding challenge, and I'll try to be regular at it.
 Please follow them serially.
