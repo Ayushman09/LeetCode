2f18e73027b0336ebad793b4c2658926fc94e74e
Ayushman09 <ayushmanchak@gmail.com>
2022-04-03 12:34:41 +0530

    April 3rd- Q.31-added binary

--- 31. Next Permutation.py
+++ 31. Next Permutation.py
@@ -16,3 +16,31 @@ def nextPermutation(self, nums: List[int]) -> None:
         while l < r:
             nums[l], nums[r] = nums[r], nums[l]
             l +=1 ; r -= 1
+
+#new soln using binary search
+def nextPermutation(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        i = n - 1
+        while i > 0 and nums[i-1] >= nums[i]:
+            i -= 1
+
+        # use binary search because it's descending order
+        if i > 0:
+            left, right = i, n - 1
+            while left <= right:
+                mid = left + (right - left) // 2
+                if nums[i-1] < nums[mid]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+            nums[i-1], nums[left-1] = nums[left-1], nums[i-1]
+
+        # reverse
+        left, right = i, n - 1
+        while left < right:
+            nums[left], nums[right] = nums[right], nums[left]
+            left += 1
+            right -= 1
