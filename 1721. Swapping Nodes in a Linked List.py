7687cfa2c9256eeb2352564492a7511ff3b05dad
Ayushman09 <ayushmanchak@gmail.com>
2022-04-04 14:45:17 +0530

    April 4th- Q.1721

---
+++ 1721. Swapping Nodes in a Linked List.py
@@ -0,0 +1,10 @@
+def swapNodes(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        first = last = head
+        for i in range(1, k):
+            first = first.next
+        null_checker = first
+        while null_checker.next:
+            last = last.next
+            null_checker = null_checker.next
+        first.val, last.val = last.val, first.val
+        return head
