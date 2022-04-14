47a7f73d9b6df480e6073b9c986eaaedfd4ca8b3
Ayushman09 <ayushmanchak@gmail.com>
2022-04-14 12:30:52 +0530

    April 14th- Q.700

---
+++ 700. Search in a Binary Search Tree.py
@@ -0,0 +1,9 @@
+def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        if not root:
+            return None
+        if root.val == val:
+            return root
+        elif root.val > val:
+            return self.searchBST(root.left, val)           
+        else:
+            return self.searchBST(root.right, val)
