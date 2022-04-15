74a24a68f7a18fafd6491712266db9f1c58a7052
Ayushman09 <ayushmanchak@gmail.com>
2022-04-15 11:29:11 +0530

    April 15th- Q.669

---
+++ 669. Trim a Binary Search Tree.py
@@ -0,0 +1,10 @@
+def trimBST(self, root: Optional[TreeNode], L: int, R: int) -> Optional[TreeNode]:
+        if not root:
+            return None
+        if L > root.val:
+            return self.trimBST(root.right, L, R)
+        elif R < root.val:
+            return self.trimBST(root.left, L, R)
+        root.left = self.trimBST(root.left, L, R)
+        root.right = self.trimBST(root.right, L, R)
+        return root
