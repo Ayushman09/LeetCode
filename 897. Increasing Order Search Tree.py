a6d8d65b7b3ec1811080bb13fb2bc4f704beddd4
Ayushman09 <ayushmanchak@gmail.com>
2022-04-17 15:36:36 +0530

    April 17th- Q.897

---
+++ 897. Increasing Order Search Tree.py
@@ -0,0 +1,6 @@
+def increasingBST(self, root: TreeNode, tail=None) -> TreeNode:
+        if not root: return tail
+        res = self.increasingBST(root.left, root)
+        root.left = None
+        root.right = self.increasingBST(root.right, tail)
+        return res
