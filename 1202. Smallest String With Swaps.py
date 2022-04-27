6bb3850753af5ecd03140124247862b145f16f31
Ayushman09 <ayushmanchak@gmail.com>
2022-04-27 10:30:29 +0530

    April 27th- Q.1202

---
+++ 1202. Smallest String With Swaps.py
@@ -0,0 +1,22 @@
+def smallestStringWithSwaps(self, s: str, pairs: List[List[int]]) -> str:
+        d = defaultdict(list)
+        for a,b in pairs:
+            d[a].append(b)
+            d[b].append(a)
+        #
+        def dfs(x,A):
+            if x in d:
+                A.append(x)
+                for y in d.pop(x):
+                    dfs(y,A)
+        #
+        s    = list(s)
+        while d:
+            x = next(iter(d))
+            A = []
+            dfs(x,A)
+            A = sorted(A)
+            B = sorted([ s[i] for i in A ])
+            for i,b in enumerate(B):
+                s[A[i]] = b
+        return ''.join(s)
