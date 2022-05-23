07a1a10abf9153dbc5d081a15ae73a26b4425b63
Ayushman09 <ayushmanchak@gmail.com>
2022-05-23 12:36:18 +0530

    March 23rd - Q.474

---
+++ 474. Ones and Zeroes.py
@@ -0,0 +1,10 @@
+def findMaxForm(self, strs: List[str], m: int, n: int) -> int:
+        dp = [[ 0 ] * (n+1) for _ in range(m+1)]
+        for s in strs:
+            zeros, ones = s.count("0"), s.count("1")
+            for i in range(m, zeros - 1, -1):
+                for j in range(n, ones - 1, -1):
+                    # dp[i][j] indicates it has i zeros and j ones, can this string be formed with those ?
+                    dp[i][j] = max( 1 + dp[i - zeros][j- ones], dp[i][j] )
+            # print(dp)
+        return dp[-1][-1]
