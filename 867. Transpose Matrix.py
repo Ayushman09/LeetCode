4a18047b9202b94e6ac3073180af1ba8cf61ed15
Ayushman09 <ayushmanchak@gmail.com>
2022-06-03 09:05:21 +0530

    June 2nd - Q.867

---
+++ 867. Transpose Matrix.py
@@ -0,0 +1,11 @@
+def transpose(self, matrix: List[List[int]]) -> List[List[int]]:
+        sol = []
+        
+        for c in range(len(matrix[0])):
+            tmp = []
+            for r in range(len(matrix)):
+                tmp.append(matrix[r][c])
+            
+            sol.append(tmp)
+        
+        return sol
