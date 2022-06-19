4dcd54ae8f4a6807b1748d38bf3fc3686199f9fd
Ayushman09 <ayushmanchak@gmail.com>
2022-06-19 14:24:42 +0530

    June 19th - Q.1268

---
+++ 1268. Search suggestions system.py
@@ -0,0 +1,9 @@
+#pure python magic solution
+def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
+        products.sort()
+        res, prefix, i = [], '', 0
+        for c in searchWord:
+            prefix += c
+            i = bisect.bisect_left(products, prefix, i)
+            res.append([w for w in products[i:i + 3] if w.startswith(prefix)])
+        return res
