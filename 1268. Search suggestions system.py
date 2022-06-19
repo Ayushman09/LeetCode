067b2dc27b0f6646a6764bb89c4c1e68c75b053f
Ayushman09 <ayushmanchak@gmail.com>
2022-06-19 14:25:40 +0530

    June 19th - Q.1268

--- 1268. Search suggestions system.py
+++ 1268. Search suggestions system.py
@@ -7,3 +7,28 @@ def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[s
             i = bisect.bisect_left(products, prefix, i)
             res.append([w for w in products[i:i + 3] if w.startswith(prefix)])
         return res
+
+# more of a DSA enthusiast pythonic solution - Trie+Sort
+def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
+        class TrieNode:
+            def __init__(self):
+                self.children = collections.defaultdict(TrieNode)
+                self.suggestion = []
+            
+            def add_sugestion(self, product):
+                if len(self.suggestion) < 3:
+                    self.suggestion.append(product)
+        
+        products = sorted(products)
+        root = TrieNode()
+        for p in products:
+            node = root
+            for char in p:
+                node = node.children[char]
+                node.add_sugestion(p)
+        
+        result, node = [], root
+        for char in searchWord:
+            node = node.children[char]
+            result.append(node.suggestion)
+        return result
